bc2aab7b6d7ccd2bfb2df0eeeae6bcd200be727a
Raoji Patil <raojipatil22@gmail.com>
2021-11-07 22:24:50 +0530

    navbar file

---
+++ Car Booking page/component/navbar.js
@@ -0,0 +1,15 @@
+function navbar() {
+    return `  <div id="navbar">
+    <h3 id="logo">
+        <a href="#"><img src="https://www.zoomcar.com/build/98e56e8b0b91e8806885a22ac2bf69a7.png" alt=""></a>
+    </h3>
+
+    <div id="options">
+        <h3><a id="navbar_color" href="#">Become a Host</a></h3>
+        <h3><a id="navbar_color" href="#">ZMS</a></h3>
+        <h3><a id="navbar_color" href="#">Login/Signup</a></h3>
+    </div>
+</div>`
+}
+
+export default navbar
