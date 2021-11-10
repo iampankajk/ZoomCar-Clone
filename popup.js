069f9c899756f0a2a65868376cf8f497c1f888b1
Nikita Parekh <nikitaparekh00@gmail.com>
2021-11-10 18:20:11 +0530

    updated popup function

--- popup.js
+++ popup.js
@@ -5,11 +5,11 @@ const closeBtn = document.querySelector(".close-btn");
 window.addEventListener("load", () => {
   setTimeout(() => {
     popupScreen.classList.add("active");
-  }, 1000); //Popup the screen in 02 seconds after the page is loaded.
+  }, 1000); //Popup the screen in 01 second after the page is loaded.
 });
 
 closeBtn.addEventListener("click", () => {
   popupScreen.classList.remove("active"); //Close the popup screen on click the close button.
 });
 
-popupScreen.style.display = "flex"; //Show the popup screen if the cookie is expired.
+popupScreen.style.display = "flex"; //Show the popup screen.
